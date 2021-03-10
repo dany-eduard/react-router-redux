a5c00bad331b7a48076ce71b02cc03054834b9bc
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-03-10 09:12:51 -0500

    Update: comentario

--- src/components/Civilizacion.jsx
+++ src/components/Civilizacion.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 
 const Civilizacion = () => {
-  const { id } = useParams();
+  const { id } = useParams(); // Se obtiene el parámetro id de la URL
   console.log(id);
   const [civilizacion, setCivilizacion] = useState([]);
 
