0c8275453e5da16a6d0ec4711abf48d5f3b5809d
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-03-10 15:59:57 -0500

    Agrega funcionalidad para obtener y mostrar más Mustra los siguientes datos obtenidos de la API usando el boton <Ver más>

--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -3,31 +3,57 @@ import axios from "axios";
 // Constantes
 const dataInicial = {
   datos: [],
+  offset: 0,
 };
 
 // Types
 const OBTENER_POKES_EXITO = "OBTENER_POKES_EXITO";
+const OBTENER_SIGUIENTE_POKES_EXITO = "OBTENER_SIGUIENTE_POKES_EXITO";
 
 // Reducers
 export default function pokeReducer(state = dataInicial, action) {
   switch (action.type) {
     case OBTENER_POKES_EXITO:
       return { ...state, datos: action.payload };
-
+    case OBTENER_SIGUIENTE_POKES_EXITO:
+      return {
+        ...state,
+        datos: action.payload.datos,
+        offset: action.payload.offset,
+      };
     default:
       return state;
   }
 }
 
 // Acciones
-export const obtenerPokesAccion = () => async (dispatch, getState) => {
+export const obtenerPokesAccion = () => async (dispatch) => {
   try {
     const res = await axios.get(
-      "https://pokeapi.co/api/v2/pokemon?offset=0&limit=50"
+      "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
     );
     dispatch({
       type: OBTENER_POKES_EXITO,
       payload: res.data.results,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const siguientePokesAccion = () => async (dispatch, getState) => {
+  // console.log("getState", getState().pokes.offset);
+  const siguiente = getState().pokes.offset + 20;
+  try {
+    const res = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon?offset=${siguiente}&limit=20`
+    );
+    dispatch({
+      type: OBTENER_SIGUIENTE_POKES_EXITO,
+      payload: {
+        datos: res.data.results,
+        offset: siguiente,
+      },
     });
   } catch (error) {
     console.log(error);
