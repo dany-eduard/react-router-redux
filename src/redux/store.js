246473cdf8f3b94a41a3e421254192477940ed20
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-03-10 16:04:18 -0500

    Conecta con la extension Redux Devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,11 @@ import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import pokeReducer from "./pokeDucks";
 
-const rootReducer = combinarReducers({
+const rootReducer = combineReducers({
   pokes: pokeReducer,
 });
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // Conecta con la extension Redux Devtools
 
 export default function generateStore() {
   const store = createStore(
